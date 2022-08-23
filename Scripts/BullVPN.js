131818c5ded07bf228a99545a8f95d52e9e4c319
GalaxySpace <49269418+GalaxySpace@users.noreply.github.com>
2022-08-23 19:23:35 +0800

    Update BullVPN.js

--- Scripts/BullVPN.js
+++ Scripts/BullVPN.js
@@ -16,12 +16,10 @@ hostname = client-api.blackbull.news
 ***********************************/
 
 var url = $request.url;
-var objc = JSON.parse($response.body);
 const tmp1 = '/v2/user/info';
 const tmp2 = '/v2/session/connect';
 
 if (url.indexOf(tmp1) != -1) {
 	var body = $response.body.replace(/type:\w+/g,'type:pay').replace(/is_pay":\d+/g,'is_pay":1').replace(/days":\d+/g,'days":99999999').replace(/end":".*?"/g,'end":"2999-09-09 09:09:09"');
 }
-body = JSON.stringify(objc);
 $done({body});
