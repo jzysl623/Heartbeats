c70869e05417680f1f5b6b4ceb4e817859e3271a
GalaxySpace <49269418+GalaxySpace@users.noreply.github.com>
2022-08-26 07:33:21 +0800

    Create VPN Plus.js

---
+++ Scripts/VPN Plus.js
@@ -0,0 +1,24 @@
+/***********************************
+> 應用名稱：BullVPN
+> 軟件版本：6.2
+> 腳本作者：Yakun
+> 更新時間：2022-08-26
+> 腳本功能：⛔无限体验⛔
+> 特别說明：本腳本僅供學習交流使用，禁止售賣
+ 
+[rewrite_local]
+
+# VPN Plus（2022-08-26)
+^https?:\/\/206.189.78.230\/receipt\/apple url script-response-body https://raw.githubusercontent.com/GalaxySpace/Heartbeats/main/Scripts/BullVPN.js
+[mitm] 
+hostname = 206.189.78.230
+
+***********************************/
+
+var url = $request.url;
+const tmp1 = '/receipt/apple';
+
+if (url.indexOf(tmp1) != -1) {
+	var body = $response.body.replace(/ExpireTime:\d+/g,'ExpireTime:92503620603');
+}
+$done({body});
