0c2e0af43f0ea59df850d0d9a95b2013af22735d
GalaxySpace <49269418+GalaxySpace@users.noreply.github.com>
2022-08-26 11:34:28 +0800

    Update verifyReceipt.js

--- Scripts/verifyReceipt.js
+++ Scripts/verifyReceipt.js
@@ -205,79 +205,40 @@ objc = {
 // Picsew
 if (ua.indexOf('Picsew') != -1) {
 objc = {
-    "environment": "Production",
     "receipt": {
-        "receipt_type": "Production",
-        "adam_id": 14396865180,
-        "app_item_id": 14396865180,
-        "bundle_id": "com.klink.proxy",
-        "application_version": "2",
-        "download_id": 500920174996467000,
-        "version_external_identifier": 849955888,
-        "receipt_creation_date": "2022-08-04 02:33:04 Etc/GMT",
-        "receipt_creation_date_ms": "1659580384000",
-        "receipt_creation_date_pst": "2022-08-03 19:33:04 America/Los_Angeles",
-        "request_date": "2022-08-04 02:33:07 Etc/GMT",
-        "request_date_ms": "1659580387173",
-        "request_date_pst": "2022-08-03 19:33:07 America/Los_Angeles",
-        "original_purchase_date": "2021-11-26 05:25:44 Etc/GMT",
-        "original_purchase_date_ms": "1637904344000",
-        "original_purchase_date_pst": "2021-11-25 21:25:44 America/Los_Angeles",
-        "original_application_version": "5",
-        "in_app": [
-            {
-                "quantity": "1",
-                "product_id": "com.shadowlink.subscription.year",
-                "transaction_id": "500001131964986",
-                "original_transaction_id": "500001131964986",
-                "purchase_date": "2022-08-04 02:33:03 Etc/GMT",
-                "purchase_date_ms": "1659580383000",
-                "purchase_date_pst": "2022-08-03 19:33:03 America/Los_Angeles",
-                "original_purchase_date": "2022-08-04 02:33:04 Etc/GMT",
-                "original_purchase_date_ms": "1659580384000",
-                "original_purchase_date_pst": "2022-08-03 19:33:04 America/Los_Angeles",
-                "expires_date": "2999-08-07 02:33:03 Etc/GMT",
-                "expires_date_ms": "32490988383000",
-                "expires_date_pst": "2999-08-06 19:33:03 America/Los_Angeles",
-                "web_order_line_item_id": "500000507444528",
-                "is_trial_period": "true",
-                "is_in_intro_offer_period": "false",
-                "in_app_ownership_type": "PURCHASED"
-            }
-        ]
-    },
-    "latest_receipt_info": [
-        {
-            "quantity": "1",
-            "product_id": "com.shadowlink.subscription.year",
-            "transaction_id": "500001131964986",
-            "original_transaction_id": "500001131964986",
-            "purchase_date": "2022-08-04 02:33:03 Etc/GMT",
-            "purchase_date_ms": "1659580383000",
-            "purchase_date_pst": "2022-08-03 19:33:03 America/Los_Angeles",
-            "original_purchase_date": "2022-08-04 02:33:04 Etc/GMT",
-            "original_purchase_date_ms": "1659580384000",
-            "original_purchase_date_pst": "2022-08-03 19:33:04 America/Los_Angeles",
-            "expires_date": "2999-08-07 02:33:03 Etc/GMT",
-            "expires_date_ms": "32490988383000",
-            "expires_date_pst": "2999-08-06 19:33:03 America/Los_Angeles",
-            "web_order_line_item_id": "500000507444528",
-            "is_trial_period": "true",
-            "is_in_intro_offer_period": "false",
-            "in_app_ownership_type": "PURCHASED",
-            "subscription_group_identifier": "20601499"
-        }
-    ],
-    "latest_receipt": null,
-    "pending_renewal_info": [
-        {
-            "auto_renew_product_id": "com.shadowlink.subscription.year",
-            "product_id": "com.shadowlink.subscription.year",
-            "original_transaction_id": "500001131964986",
-            "auto_renew_status": "1"
-        }
-    ],
-    "status": 0
+    "receipt_type": "Production",
+    "adam_id": 120814516700,
+    "app_item_id": 120814516700,
+    "bundle_id": "com.sugarmo.ScrollClip",
+    "application_version": "3082",
+    "download_id": 999999,
+    "version_external_identifier": 837747342,
+    "receipt_creation_date": "2020-09-13 06:58:34 Etc/GMT",
+    "receipt_creation_date_ms": "1599980314000",
+    "receipt_creation_date_pst": "2020-09-12 23:58:34 America/Los_Angeles",
+    "request_date": "2020-09-13 06:59:15 Etc/GMT",
+    "request_date_ms": "1599980355799",
+    "request_date_pst": "2020-09-12 23:59:15 America/Los_Angeles",
+    "original_purchase_date": "2020-09-13 06:50:28 Etc/GMT",
+    "original_purchase_date_ms": "1599979828000",
+    "original_purchase_date_pst": "2020-09-12 23:50:28 America/Los_Angeles",
+    "original_application_version": "3082",
+    "in_app": [{
+      "quantity": "1",
+      "product_id": "com.sugarmo.ScrollClip.pro",
+      "transaction_id": "1000000000000000",
+      "original_transaction_id": "1000000000000000",
+      "purchase_date": "2020-01-01 00:00:00 Etc/GMT",
+      "purchase_date_ms": "1587700000000",
+      "purchase_date_pst": "2020-01-21 00:00:00 America/Los_Angeles",
+      "original_purchase_date": "2020-01-01 00:00:00 Etc/GMT",
+      "original_purchase_date_ms": "1587700000000",
+      "original_purchase_date_pst": "2020-01-01 00:00:00 America/Los_Angeles",
+      "is_trial_period": "false"
+    }]
+  },
+  "status": 0,
+  "environment": "Production"
 };
 }
 
