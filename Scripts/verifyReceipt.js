57728a1f80d84714efd71f3847c41b273155e882
GalaxySpace <49269418+GalaxySpace@users.noreply.github.com>
2022-08-26 11:00:08 +0800

    Update verifyReceipt.js

--- Scripts/verifyReceipt.js
+++ Scripts/verifyReceipt.js
@@ -1,7 +1,8 @@
 var head = $request.headers;
 var ua = head['User-Agent'];
 var objc = JSON.parse($response.body); 
-if (ua.indexOf('VPN') != -1) {// vpnfast
+// vpnfast
+if (ua.indexOf('VPN') != -1) {
 objc = {
 	"environment": "Production",
 	"receipt": {
@@ -120,4 +121,44 @@ objc = {
 	"status": 0
 };
 }
+
+// Picsew
+if (ua.indexOf('Picsew') != -1) {
+objc = {
+  "receipt": {
+    "receipt_type": "Production",
+    "adam_id": 120814516700,
+    "app_item_id": 120814516700,
+    "bundle_id": "com.sugarmo.ScrollClip",
+    "application_version": "3082",
+    "download_id": 999999,
+    "version_external_identifier": 837747342,
+    "receipt_creation_date": "2020-09-13 06:58:34 Etc/GMT",
+    "receipt_creation_date_ms": "1599980314000",
+    "receipt_creation_date_pst": "2020-09-12 23:58:34 America/Los_Angeles",
+    "request_date": "2020-09-13 06:59:15 Etc/GMT",
+    "request_date_ms": "1599980355799",
+    "request_date_pst": "2020-09-12 23:59:15 America/Los_Angeles",
+    "original_purchase_date": "2020-09-13 06:50:28 Etc/GMT",
+    "original_purchase_date_ms": "1599979828000",
+    "original_purchase_date_pst": "2020-09-12 23:50:28 America/Los_Angeles",
+    "original_application_version": "3082",
+    "in_app": [{
+      "quantity": "1",
+      "product_id": "com.sugarmo.ScrollClip.pro",
+      "transaction_id": "1000000000000000",
+      "original_transaction_id": "1000000000000000",
+      "purchase_date": "2020-01-01 00:00:00 Etc/GMT",
+      "purchase_date_ms": "1587700000000",
+      "purchase_date_pst": "2020-01-21 00:00:00 America/Los_Angeles",
+      "original_purchase_date": "2020-01-01 00:00:00 Etc/GMT",
+      "original_purchase_date_ms": "1587700000000",
+      "original_purchase_date_pst": "2020-01-01 00:00:00 America/Los_Angeles",
+      "is_trial_period": "false"
+    }]
+  },
+  "status": 0,
+  "environment": "Production"
+}
+
 $done({body:JSON.stringify(objc)});
